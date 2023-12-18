15bc76367724630f261a631429ce5676b4b3ed5e
shouryasharma <shouryasharma@gmail.com>
2023-12-18 17:44:13 +0530

    binary search

---
+++ Python.py
@@ -0,0 +1,19 @@
+# input & variable
+name = input("enter your name: ")
+# type casting
+age = int(input("enter your age: "))
+
+# print
+print(name, "is", age, "years old")
+
+# type of variable
+print(type(name))
+print(type(age))
+
+# floor division
+print(60//9)
+
+str = "ABCD"
+print(len(str))
+print(str[3])
+print(str[-1])
